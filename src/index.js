73b49f9d2b58fdea96bfec520bdcfdf620b48976
Veronica Andrade <veronica726@gmail.com>
2020-01-13 14:47:37 -0700

    Add functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-
+// import './main-client'
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
-
+import '../src/css/base.scss'
+import '../src/main-client'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/cherry-blossom-background-4.jpg'
+import Hotel from './Hotel';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let hotel;
+let userId;
+
+$('.user-login-button').click(findOutWhoTheUserIs)
+$('.manager-login-button').click(findOutWhoTheUserIs)
+// $('.user-login-button').click(resolveAllPromises)
+// $('.manager-login-button').click(resolveAllPromises)
+// $('.')
+
+function findOutWhoTheUserIs() {
+  let userValue = $('.user-input-bar').val();
+  if(userValue === 'manager') {
+    return 'manager';
+  } else if (userValue.includes('customer')) {
+    let userInput = userValue.split('r')
+    userId = userInput[1];
+    window.location.href = './client.html?userId=' + userId
+  }
+}
+
+function displayClientPage(userId) {
+}
+
+function grabAllBookings(userId) {
+
+}
+console.log('index.js is here')
